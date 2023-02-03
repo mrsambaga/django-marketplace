c5afaf753e96d4e78c8bb9e18f9683a481d78235
Sam <m.r.sambaga@gmail.com>
2023-02-03 23:52:56 +0700

    add item app

--- marketplace_project/marketplace_app/views.py
+++ marketplace_project/marketplace_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from item.models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'marketplace_app/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    category = Category.objects.all()
+    return render(request, 'marketplace_app/index.html', {'items' : items, 'category' : category})
 
 def contact(request):
-    pass
+    return render(request, 'marketplace_app/contact.html')
