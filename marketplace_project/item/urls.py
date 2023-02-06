209a778ccd7baf19c66bc65341c29060ba4845f5
Sam <m.r.sambaga@gmail.com>
2023-02-06 14:14:08 +0700

    add sign in & new item page

--- marketplace_project/item/urls.py
+++ marketplace_project/item/urls.py
@@ -5,4 +5,5 @@ app_name = 'item'
 
 urlpatterns = [
     path('items/<int:pk>/', views.detail, name='detail'),
+    path('new/', views.new, name="new"),
 ]
--- marketplace_project/marketplace_app/urls.py
+++ marketplace_project/marketplace_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
+from .forms import *
 
 app_name = 'marketplace_app'
 
@@ -10,4 +12,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('contact/', views.contact, name="contact"),
     path('signup/', views.signup, name="signup"),
+    path('login/', auth_views.LoginView.as_view(template_name='marketplace_app/login.html', authentication_form=LoginForm), name="login"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
